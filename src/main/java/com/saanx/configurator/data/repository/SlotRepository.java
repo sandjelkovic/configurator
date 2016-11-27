0b7809c352ff719ff85023dc7adb4af1daeee611
stefan <sa.stefan.andjelkovic@gmail.com>
2016-11-27 18:17:24 +0100

    Do not export Slot repository

--- src/main/java/com/saanx/configurator/data/repository/SlotRepository.java
+++ src/main/java/com/saanx/configurator/data/repository/SlotRepository.java
@@ -5,6 +5,6 @@ import com.saanx.configurator.data.entity.Slot;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource()
+@RepositoryRestResource(exported = false)
 public interface SlotRepository extends PagingAndSortingRepository<Slot, Long> {
 }
