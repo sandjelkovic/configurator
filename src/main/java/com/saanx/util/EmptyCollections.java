7980f048eb77dbdeedb7829e045393c900473830
stefan <sa.stefan.andjelkovic@gmail.com>
2017-02-18 10:35:30 +0100

    Update EmptyCollections

--- src/main/java/com/saanx/util/EmptyCollections.java
+++ src/main/java/com/saanx/util/EmptyCollections.java
@@ -1,21 +1,30 @@
 package com.saanx.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class EmptyCollections {
-	public static List list() {
-		return new ArrayList<>();
+	public static <T> List<T> list() {
+		return new ArrayList<T>();
 	}
 
-	public static Map map() {
+	public static <T, R> Map<T, R> map() {
 		return new HashMap<>();
 	}
 
-	public static <T> Set<T> set() {
-		return new HashSet<T>();
+	public static Set set() {
+		return new HashSet<>();
 	}
 
-	public static List emptyIfNull(List list) {
+	public static <T> List<T> emptyIfNull(List<T> list) {
 		return (list != null) ? list : list();
+	}
+
+	public static <T, R> Map<T, R> emptyIfNull(Map<T, R> map) {
+		return (map != null) ? map : map();
 	}
 }
